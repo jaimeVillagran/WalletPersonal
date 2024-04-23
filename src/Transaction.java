30a848448401e52824e1227b6482b235c7150ff0
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-23 15:47:15 -0400

    Actualizar clase Transaction para hacer que los campos sean finales y agregar campos finales a la clase Usuario

--- src/Transaction.java
+++ src/Transaction.java
@@ -4,19 +4,14 @@ public class Transaction {
     private final String type;
     private final double amount;
     private final LocalDateTime date;
-    private final String description;
-    private final double balanceAfter;
 
-
-    public Transaction(String type, double amount, String description, double balanceAfter) {
+    public Transaction(String type, double amount) {
         this.type = type;
         this.amount = amount;
         this.date = LocalDateTime.now();
-        this.description = description;
-        this.balanceAfter = balanceAfter;
     }
 
-    // Métodos getters para los campos
+    // Métodos para obtener los atributos de la transacción
     public String getType() {
         return type;
     }
@@ -27,13 +22,5 @@ public class Transaction {
 
     public LocalDateTime getDate() {
         return date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getBalanceAfter() {
-        return balanceAfter;
     }
 }
