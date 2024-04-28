12427ba78119e10f69a9f067f716e4fac7c29359
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-27 20:02:46 -0400

    Eliminar clases ChileanWallet y OperationsMenu

--- src/User.java
+++ src/User.java
@@ -1,19 +1,24 @@
 public class User {
     private String name;
     private String password;
+    private String email;
     private Wallet wallet;
 
-    public User(String name, String password, Wallet wallet) {
+    public User(String name, String password) {
         this.name = name;
         this.password = password;
-        this.wallet = wallet;
+        this.wallet = new Wallet(); // Add parentheses here
     }
 
     public Wallet getWallet() {
         return wallet;
     }
 
-    public boolean authenticate(String password) {
-        return this.password.equals(password);
+    public String getName() {
+        return name;
+    }
+
+    public boolean checkPassword(String inputPassword) {
+        return password.equals(inputPassword);
     }
 }
