0361911823cade407d6f9fcb5ec6c5a001cf902e
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-27 20:31:30 -0400

    Actualizar clase Wallet para utilizar una lista de transacciones en lugar de un balance en CLP

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,33 +1,62 @@
-import Currency;
+import java.util.List;
 
 public abstract class Wallet {
-  private final Currency chosenCurr = Currency.getInstance("CLP"); // Moneda seleccionada por el usuario
-  protected double balanceCLP; // Saldo en pesos chilenos (CLP)
-  protected CurrencyManager currencyManager; // Manejo de monedas
+  private double balance;
+  private final List<Transaction> transactions;
 
-  public Wallet(CurrencyManager currencyManager) {
-    this.balanceCLP = 0;
-    this.currencyManager = currencyManager;
+  public Wallet() {
+    this.balance = 0;
+    this.transactions = new ArrayList<>();
   }
 
-  public Wallet(User user, Currency chosenCurrency) {
-    this.balanceCLP = 0;
-    this.currencyManager = new CurrencyManager();
-    this.currencyManager.addCurrency(Currency);
+  public void deposit(double amount) {
+    if (amount > 0) {
+      Deposit deposit = new Deposit(amount); // Crea un nuevo objeto de la clase
+      transactions.add(deposit); // Agrega la transacción al historial de
+      balance += amount; // Actualiza el saldo de la billeter
+      System.out.println("Depósito exitoso."); // Imprime el mensaje de exito
+      System.out.println(deposit.getDescription()); // Imprime la descripción
+    } else {
+      System.out.println("No se puede realizar el depósito."); // Imprime el mensaje de error
+      System.out.println("La cantidad a depositar debe ser mayor a 0."); // Imprime el mensaje de error
+    }
+
   }
 
-  public abstract void deposit(double amount); // Método abstracto para depósito en CLP
+  public void withdraw(double amount) {
+    if (amount <= balance && amount > 0) {
+      Withdrawal withdrawal = new Withdrawal(amount); // Crea un nuevo objeto de la clase
+      transactions.add(withdrawal); // Agrega la transacción al historial de
+      balance -= amount; // Actualiza el saldo de la billeter
+      System.out.println("Retiro exitoso."); // Imprime el mensaje de exito
+      System.out.println(withdrawal.getDescription()); // Imprime la descripción
+    } else {
+      System.out.println("No se puede realizar el retiro."); // Imprime el mensaje de error
+      System.out.println("La cantidad a retirar debe ser mayor a 0 y menor o igual al saldo."); // Imprime el mensaje de
+                                                                                                // error
+    }
 
-  public abstract void withdraw(double amount); // Método abstracto para retiro en CLP
+  }
 
   public double getBalance() {
-    return balanceCLP; // Devuelve el saldo en CLP
+    return balance; // Devuelve el saldo actual
   }
 
-  public void checkBalance(Currency currency) {
-    // Convierte el saldo a la moneda deseada
-    double convertedBalance = currencyManager.convertFromCLP(balanceCLP, currency);
-    System.out.println(
-        "Saldo actual en " + currency.getCurrencyCode() + ": " + currency.getSymbol() + " " + convertedBalance);
+  public void checkBalance() {
+    System.out.println("****************");
+    System.out.println("* Billetera Wallet Personal *");
+    System.out.println("****************");
+    System.out.println("Saldo actual: " + balance);
+    System.out.println("****************");
   }
+
+  public void showTransactions() {
+    System.out.println("****************");
+    System.out.println("* Transacciones *");
+    System.out.println("****************");
+    for (Transaction transaction : transactions) {
+      System.out.println(transaction.getDescription());
+    }
+  }
+
 }
