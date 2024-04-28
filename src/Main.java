b7aecdf1b61ee9eccec92477b1196fd665f9fcad
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-27 20:10:39 -0400

    Actualizar clase Main para mostrar un mensaje de bienvenida y utilizar la clase Menu

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear CurrencyManager y agregar monedas disponibles
-        CurrencyManager currencyManager = new CurrencyManager();
-        currencyManager.addCurrency(new USD());
-        currencyManager.addCurrency(new EUR());
-        currencyManager.addCurrency(new GBP());
-        currencyManager.addCurrency(new CLP());
+        System.out.println("****************");
+        System.out.println("* Bienvenido a la billetera Wallet Personal *");
+        System.out.println("****************");
 
-        // Crear la billetera del usuario en CLP
-        Wallet wallet = new ChileanWallet(currencyManager);
-
-        // Crear un usuario
-        User user = new User("Juan", "1234", wallet);
-
-        // Crear un escáner para leer la entrada del usuario
-        Scanner sc = new sc(System.in);
-
-        // Crear una instancia de autenticación
-        Authentication authentication = new Authentication(user, sc);
+        // Crear una instancia de Authentication
+        Authentication auth = new Authentication();
 
         // Realizar la autenticación
-        if (authentication.authenticate()) {
-            // Crear el menú de operaciones
-            OperationsMenu operationsMenu = new OperationsMenu(user, wallet, currencyManager, sc);
-            // Mostrar el menú de operaciones
-            operationsMenu.showMenu();
+        if (!auth.authenticate()) {
+
+            // Cerrar el escáner al salir
+            auth.closeScanner();
+            return;
         }
 
+        // Obtener usuario autenticado
+        User predefinedUser = auth.getPredefinedUser();
+
+        // Definir tipo de cambio
+        double exchangeRate = 950.0; // CLP por USD
+
+        // Obetner instancia de Menu
+        Menu menu = new Menu(predefinedUser, exchangeRate);
+
+        // Mostrar el menú
+        menu.displayMenu();
+
         // Cerrar el escáner al salir
-        sc.close();
+        auth.closeScanner();
     }
 }
