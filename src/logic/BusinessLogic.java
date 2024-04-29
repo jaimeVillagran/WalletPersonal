9a3010aa77e4c4993d3eb12de31ac5013c25defc
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-29 13:05:59 -0400

    Creacion de clase BusinessLogic

---
+++ src/logic/BusinessLogic.java
@@ -0,0 +1,41 @@
+package logic;
+
+public class BusinessLogic {
+  private TransactionManager transactionManager;
+  private Wallet wallet;
+  private double exchangeRate;
+
+  public BusinessLogic(Wallet wallet, TransactionManager transactionManager, double exchangeRate) {
+    this.wallet = wallet;
+    this.transactionManager = transactionManager;
+    this.exchangeRate = exchangeRate;
+  }
+
+  // Método para realizar un depósito
+  public void deposit(double amount) {
+    transactionManager.deposit(amount);
+  }
+
+  // Método para realizar un retiro
+  public void withdraw(double amount) {
+    transactionManager.withdraw(amount);
+  }
+
+  // Método para consultar el saldo en CLP
+  public void checkBalance() {
+    System.out.println("********************************");
+    System.out.println("Saldo actual en moneda original: CLP " + String.format("%.2f", wallet.getBalance()));
+    System.out.println("********************************");
+  }
+
+  // Método para consultar el saldo en USD
+  public void checkBalanceInUSD() {
+    double balanceInCLP = wallet.getBalance();
+    double balanceInUSD = balanceInCLP / exchangeRate;
+    System.out.println("********************************");
+    System.out.println("Saldo actual en dólares: USD " + String.format("%.2f", balanceInUSD));
+    System.out.println("********************************");
+  }
+
+  // Otros métodos de lógica de negocio pueden ser agregados aquí
+}
