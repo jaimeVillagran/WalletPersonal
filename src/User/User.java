b7a95a2125202717de97d07773f21f150756f1c8
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-29 14:03:26 -0400

    Actualizar clase User para utilizar los campos de nombre y apellido en lugar de nombre completo

--- src/User/User.java
+++ src/User/User.java
@@ -1,26 +1,59 @@
 package User;
 
 public class User {
-    private String name;
-    private String password;
+    private String firstname;
+    private String lastname;
     private String email;
+    private String password;
     private Wallet wallet;
 
-    public User(String name, String password) {
-        this.name = name;
+    // Constructor para crear un usuario con nombre, apellido, email y contraseña
+
+    public User(String firstname, String lastname, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
         this.password = password;
-        this.wallet = new Wallet(); // Add parentheses here
+        this.wallet = new Wallet(); // Crea una nueva billetera para el usuario
+
     }
 
+    // Getters y setters
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter para la billetera del usuario
     public Wallet getWallet() {
         return wallet;
     }
 
-    public String getName() {
-        return name;
+    // Metodo para verificar la contraseña del usuario
+
+    public boolean checkPassword(String inputpassword) {
+        return this.password.equals(password);
     }
 
-    public boolean checkPassword(String inputPassword) {
-        return password.equals(inputPassword);
-    }
 }
