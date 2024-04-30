22a505fac433e3e008cb0880b4f25e9a92fceea0
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-04-30 17:27:31 -0400

    Actualizar autenticación de usuario y creación de cuenta en Main.java

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,15 +1,24 @@
 package App;
 
+import authentication.UserAuthenticator;
 import logic.BusinessLogic;
 import menu.Menu;
-import menu.UserInteraction;
 import transactions.TransactionManager;
-import User.User;
-import User.Wallet;
+import user.User;
+import user.UserAccountManager;
+import user.Wallet;
 
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * La función main de la aplicación Wallet Personal.
+     * Esta clase es responsable de iniciar la aplicación, autenticar al usuario,
+     * crear cuentas de usuario y depositar, retirar y mostrar el menú para la
+     * interacción del usuario.
+     *
+     * @param args los argumentos de línea de comandos
+     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -48,27 +57,6 @@ public class Main {
             sc.close();
             return;
         }
-
-        /*
-         * / // Crear una instancia de UserInteraction para manejar la interacción
-         * inicial
-         * // con el usuario
-         * UserInteraction userInteraction = new UserInteraction(sc);
-         * 
-         * // Pedir al usuario que ingrese si tiene una cuenta existente o desea crear
-         * una
-         * // nueva
-         * User user = userInteraction.getUser();
-         * 
-         * if (user == null) {
-         * // Si no se pudo obtener un usuario válido, salir de la aplicación
-         * System.out.
-         * println("No se pudo obtener un usuario válido. Cerrando la aplicación.");
-         * sc.close();
-         * return;
-         * }
-         */
-
         // Definir el tipo de cambio (CLP a USD)
         double exchangeRate = 950.0; // CLP por USD
 
@@ -76,25 +64,17 @@ public class Main {
         Wallet wallet = user.getWallet();
         TransactionManager transactionManager = new TransactionManager(wallet);
 
+        // Crear una instancia de BusinessLogic
+        BusinessLogic businessLogic = new BusinessLogic(wallet, transactionManager, exchangeRate);
+
         // Crear una instancia de Menu con BusinessLogic
         Menu menu = new Menu(businessLogic);
 
-        /*
-         * // Crear una instancia de BusinessLogic
-         * BusinessLogic businessLogic = new BusinessLogic(wallet, transactionManager,
-         * exchangeRate);
-         */
-
         // Mostrar el menú para interactuar con el usuario
-        menu.displayMenu();
-
-        // Mostrar el menú para interactuar con el usuario
-
         menu.displayMenu();
 
         // Cerrar el objeto Scanner al salir
         sc.close();
 
     }
-
 }
