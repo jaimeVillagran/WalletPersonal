093c22d7c256842c3e9e7988ec59d3ddd7a5d88c
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-05-06 23:12:26 -0400

    refactor: Remove unused authentication classes and interfaces

--- src/authentication/IUserService.java
+++ src/authentication/IUserService.java
@@ -1,9 +1,28 @@
 package authentication;
 
+/**
+ * Esta interfaz define los métodos para autenticar y crear nuevos usuarios.
+ */
 public interface IUserService {
 
+  /**
+   * Autentica a un usuario utilizando su correo electrónico y contraseña.
+   *
+   * @param email    el correo electrónico del usuario
+   * @param password la contraseña del usuario
+   * @return el objeto User si la autenticación es exitosa, o null si no lo es
+   */
   User authenticateUser(String email, String password);
 
+  /**
+   * Crea un nuevo usuario con los datos proporcionados.
+   *
+   * @param firstName el nombre del usuario
+   * @param lastName  el apellido del usuario
+   * @param email     el correo electrónico del usuario
+   * @param password  la contraseña del usuario
+   * @return el objeto User creado
+   */
   User createNewUser(String firstName, String lastName, String email, String password);
 
 }
