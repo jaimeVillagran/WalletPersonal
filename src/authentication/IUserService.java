f738afc80ae738a4e26920aabe74110827732d19
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-05-07 18:00:40 -0400

    Documentacion Interface IUserService

--- src/authentication/IUserService.java
+++ src/authentication/IUserService.java
@@ -1,28 +1,43 @@
 package authentication;
 
+import models.User;
+
 /**
- * Esta interfaz define los métodos para autenticar y crear nuevos usuarios.
+ * Interfaz para el servicio de usuario.
+ * Esta interfaz define los métodos que deben ser implementados por cualquier
+ * clase que proporcione un servicio de usuario.
+ * Los métodos incluyen la autenticación de un usuario y la creación de un nuevo
+ * usuario.
  */
 public interface IUserService {
 
   /**
-   * Autentica a un usuario utilizando su correo electrónico y contraseña.
+   * Autentica a un usuario.
+   * Este método toma un correo electrónico y una contraseña, y devuelve un objeto
+   * User si la autenticación es exitosa.
+   * Si la autenticación falla, este método puede devolver null o lanzar una
+   * excepción, dependiendo de la implementación.
    *
    * @param email    el correo electrónico del usuario
    * @param password la contraseña del usuario
-   * @return el objeto User si la autenticación es exitosa, o null si no lo es
+   * @return un objeto User si la autenticación es exitosa, null o una excepción
+   *         en caso contrario
    */
   User authenticateUser(String email, String password);
 
   /**
-   * Crea un nuevo usuario con los datos proporcionados.
+   * Crea un nuevo usuario.
+   * Este método toma un nombre, un apellido, un correo electrónico y una
+   * contraseña, y devuelve un objeto User si la creación es exitosa.
+   * Si la creación falla, este método puede devolver null o lanzar una excepción,
+   * dependiendo de la implementación.
    *
    * @param firstName el nombre del usuario
    * @param lastName  el apellido del usuario
    * @param email     el correo electrónico del usuario
    * @param password  la contraseña del usuario
-   * @return el objeto User creado
+   * @return un objeto User si la creación es exitosa, null o una excepción en
+   *         caso contrario
    */
   User createNewUser(String firstName, String lastName, String email, String password);
-
 }
